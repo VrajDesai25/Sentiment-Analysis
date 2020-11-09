af0a1bc67a33d512728d5d57c454ea10657bc562
VrajDesai25 <45138676+VrajDesai25@users.noreply.github.com>
2020-11-09 18:00:23 +0530

    Add files via upload

---
+++ store/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class products(models.Model):
+	product = models.CharField(max_length=100)
+	product_description = models.CharField(max_length=1000)
+	overall_rating = models.IntegerField()
+	neg_rating = models.IntegerField(default=0)
+	product_id = models.CharField(max_length=3)
+	image = models.ImageField(upload_to='product_img', blank=True)
+	class Meta:
+		ordering = ['neg_rating']
+
+class reviews(models.Model):
+	title = models.CharField(max_length=100)
+	#product_id = models.CharField(max_length=4)
+	product_name = models.CharField(max_length=4)
+	#product =  models.ForeignKey(products, on_delete=models.CASCADE)
+	review = models.CharField(max_length=100)
+	total_score = models.IntegerField()
+	date_posted = models.DateTimeField(auto_now_add=True)
+	author = models.CharField(max_length=50)
